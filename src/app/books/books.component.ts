b6bb5ed16470b45d456e7da5498a6c7b5a4dc231
Alexandra Gomes de Jorge <49692123+LexiGomes@users.noreply.github.com>
2019-07-16 12:55:40 +0200

    Assignment part Http --> Subscribe

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,44 +1,28 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Book} from "./models/book.interface";
+import {BooksService} from "./books.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'ibs-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
-export class BooksComponent {
-  books : Book[] = [
-    {
-    "id": 100001,
-    "title": "Application Design",
-    "author": "O'Rly?",
-    "genre": "programming",
-    "img": "assets/app-head.jpg",
-    "price": 14.95,
-    "reserved": false
-    },
-    {
-      "id": 100002,
-      "title": "Remote Programming",
-      "author": "O'Rly?",
-      "genre": "misc",
-      "img": "assets/coding.jpg",
-      "price": 25.95,
-      "reserved": true
-    },
-    {
-      "id": 100003,
-      "title": "Deadline programming",
-      "author": "O'Rly?",
-      "genre": "motivation",
-      "img": "assets/coffee-code.jpg",
-      "price": 14.95,
-      "reserved": false
-    }
-  ];
+export class BooksComponent implements OnInit {
+  books: Book[];
+
   selectedBook: Book;
 
-  onSelectBook(book:Book){
+  onSelectBook(book: Book){
     this.selectedBook = book;
+  }
+  constructor(private booksService: BooksService) {
+
+  }
+
+  ngOnInit() {
+    this.booksService.getBooks().subscribe(
+      books => this.books = books
+    );
   }
 }
